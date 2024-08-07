59f37642be064193ce2a15a2aa1f2da485c36e86
xstiff <dominik7550j@gmail.com>
2024-08-07 18:28:35 +0200

    b

--- src/app/Utils/config/Constants.ts
+++ src/app/Utils/config/Constants.ts
@@ -1,3 +1,10 @@
 export const CONSTANTS = {
-    API_BASE_URL: "http://localhost:8000/"
+    API_BASE_URL: "http://localhost:8000/",
+    STRINGS: {
+        EN: {
+            FIELD_REQUIRED: "This field is required!",
+            NOT_AN_EMAIL: "This is not an email!",
+            PASSWORD_DONT_MATCH: "Password do not match ",
+        }
+    }
 }
