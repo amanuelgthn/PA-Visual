43171c2287a934b178bb9d1c06b94ff18c1ccb68
Amanuel Getahun <amanuelgthn@gmail.com>
2024-08-07 19:41:02 +0200

    ASD ASD asd

--- src/app/ForgotPass/page.tsx
+++ src/app/ForgotPass/page.tsx
@@ -1,53 +1,53 @@
-'use client';
-import { Button, Flex, Form, Input } from 'antd';
-import React from 'react';
-import './ForgotPass.scss';
-import Link from 'next/link';
-import { LeftOutlined } from '@ant-design/icons';
+'use client'
+import { Button, Flex, Form, Input } from 'antd'
+import React from 'react'
+import './ForgotPass.scss'
+import Link from 'next/link'
+import { LeftOutlined } from '@ant-design/icons'
 const ForgotPass = () => {
-  const [form] = Form.useForm();
+  const [form] = Form.useForm()
   const HandleSubmit = () => {
-    console.log(form.getFieldsValue());
-    form.resetFields();
-  };
+    console.log(form.getFieldsValue())
+    form.resetFields()
+  }
   return (
-    <Flex className="Login-Overlay" justify="center" align="center">
+    <Flex className='Login-Overlay' justify='center' align='center'>
       <Flex
         vertical
-        className="Login-Inner"
-        justify="center"
-        align="center"
+        className='Login-Inner'
+        justify='center'
+        align='center'
         gap={20}
       >
-        <Link href="/">
-          <img src="logo/logoLogin.svg" alt="Global property Logo" />
+        <Link href='/'>
+          <img src='logo/logoLogin.svg' alt='Global property Logo' />
         </Link>
 
         <h2>Forgot password?</h2>
         <p>
-          Enter your email address below. We'll send you instructions to reset
-          your password.
+          Enter your email address below. We&apos;ll send you instructions to
+          reset your password.
         </p>
-        <Form className="login-form" onFinish={HandleSubmit} form={form}>
-          <Form.Item name="email" className="login-form-item">
-            <Input placeholder="Email address" required type="email" />
+        <Form className='login-form' onFinish={HandleSubmit} form={form}>
+          <Form.Item name='email' className='login-form-item'>
+            <Input placeholder='Email address' required type='email' />
           </Form.Item>
-          <Form.Item className="login-form-item">
-            <Button type="primary" htmlType="submit">
+          <Form.Item className='login-form-item'>
+            <Button type='primary' htmlType='submit'>
               Reset
             </Button>
           </Form.Item>
         </Form>
-        <Flex style={{ width: '100%' }} justify="space-evenly">
-          <Link href="/Login">Login</Link>
-          <Link href="/Signup">Create account</Link>
+        <Flex style={{ width: '100%' }} justify='space-evenly'>
+          <Link href='/Login'>Login</Link>
+          <Link href='/Signup'>Create account</Link>
         </Flex>
-        <Link href="/">
+        <Link href='/'>
           <LeftOutlined /> Go back
         </Link>
       </Flex>
     </Flex>
-  );
-};
+  )
+}
 
-export default ForgotPass;
+export default ForgotPass
