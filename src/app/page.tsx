9396f1c7bd78bbae263af368c656d1bea60a211e
Amanuel Getahun <amanuelgthn@gmail.com>
2024-08-15 23:54:06 +0200

    changes in home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,15 +3,21 @@ import { ContactHeader } from './components/Home/ContactHeader/ContactHeader'
 import { ExploreHeader } from './components/Home/ExploreHeader/ExploreHeader'
 import { ImageHeader } from './components/Home/ImageHeader/ImageHeader'
 import { WelcomeHeader } from './components/Home/WelcomeHeader/WelcomeHeader'
+import './page.css'
+
 
 export default function Home() {
   return (
     <>
-      <PopUpWindow />
-      <ImageHeader />
-      <WelcomeHeader />
-      <ExploreHeader />
-      <ContactHeader />
+      <div className="wrapper">
+        <div className="wrapper-assist">
+          <PopUpWindow />
+          <ImageHeader />
+          <WelcomeHeader />
+        </div>
+        <ExploreHeader />
+        <ContactHeader />
+      </div>
     </>
   )
 }
