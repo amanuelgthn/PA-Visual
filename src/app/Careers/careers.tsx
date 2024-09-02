55207af9b9246a09ca514f95d7279cd1e1c0b194
GrazynaDachtera <grazynadachtera@gmail.com>
2024-09-02 14:55:30 +0200

    created careers subpage

---
+++ src/app/Careers/careers.tsx
@@ -0,0 +1,12 @@
+import CareersHeader from '../components/Careers/CareersHeader/CareersHeader'
+import CareersSectionTwo from '../components/Careers/CareersSectionTwo/CareersSectionTwo'
+import './careers.scss'
+
+export default function Careers() {
+  return (
+    <>
+      <CareersHeader />
+      <CareersSectionTwo />
+    </>
+  )
+}
---
+++ src/app/Careers/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Careers from './careers'
+
+const CareersPage = () => {
+  return (
+    <div>
+      <Careers />
+    </div>
+  )
+}
+
+export default CareersPage
