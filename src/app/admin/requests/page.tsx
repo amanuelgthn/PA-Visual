b8d324fcd37618d9a95450ec23ca26a92745e4c3
Amanuel Getahun <amanuelgthn@gmail.com>
2024-09-03 11:08:06 +0200

    feat:Added moderator request table component

--- src/app/admin/requests/page.tsx
+++ src/app/admin/requests/page.tsx
@@ -1,13 +1,66 @@
-'use client';
-import React from 'react';
+'use client'
+import React from 'react'
+import ModeratorRequest, {
+  ModeratorTypes,
+} from '../components/ModeratorRequest/ModeratorRequest'
 
-const ModeratorRequests: React.FC = () => {
+const page = () => {
+  const requests: ModeratorTypes[] = [
+    {
+      requestId: 'REQ001',
+      type: 'Edit User',
+      submittedBy: 'moderator@globalproperty.ai',
+      date: '2023-05-20',
+      status: 'Pending',
+    },
+    {
+      requestId: 'REQ002',
+      type: 'Delete Property',
+      submittedBy: 'moderator@globalproperty.ai',
+      date: '2023-05-19',
+      status: 'Approved',
+    },
+    {
+      requestId: 'REQ004',
+      type: 'Delete Property',
+      submittedBy: 'moderator@globalproperty.ai',
+      date: '2023-05-19',
+      status: 'Rejected',
+    },
+    {
+      requestId: 'REQ004',
+      type: 'Edit User',
+      submittedBy: 'moderator@globalproperty.ai',
+      date: '2023-05-20',
+      status: 'Pending',
+    },
+    {
+      requestId: 'REQ005',
+      type: 'Delete Property',
+      submittedBy: 'moderator@globalproperty.ai',
+      date: '2023-05-19',
+      status: 'Approved',
+    },
+  ]
+
+  const handleApprove = (requestId: string) => {
+    alert(`Approveed this request id ${requestId}`)
+  }
+
+  const handleReject = (requestId: string) => {
+    alert(`Rejected this request id ${requestId}`)
+  }
+
   return (
     <div>
-      <h1>Moderator Requests</h1>
-      <p>Manage moderator requests here.</p>
+      <ModeratorRequest
+        isLoading={false}
+        requests={requests}
+        onApprove={handleApprove}
+        onReject={handleReject}
+      />
     </div>
-  );
-};
+  )
+}
 
-export default ModeratorRequests;
+export default page
