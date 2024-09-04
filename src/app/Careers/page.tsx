e8e85a50780821abb49589549a43031f4a5c6f26
Grażyna Dachtera <114610448+GrazynaDachtera@users.noreply.github.com>
2024-09-04 11:29:25 +0100

    Gd/careers (#18)
    * created careers subpage
    
    * modified careers section

---
+++ src/app/Careers/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import CareersHeader from '../components/Careers/CareersHeader/CareersHeader'
+import CareersJobPositions from '../components/Careers/CareersJobPositions/CareersJobPositions'
+
+const Careers = () => {
+  return (
+    <div>
+      <CareersHeader />
+      <CareersJobPositions />
+    </div>
+  )
+}
+
+export default Careers
