93d972d2b55bb0f0f473bc2c78ba6c28bfc854a8
Amanuel Getahun <amanuelgthn@gmail.com>
2024-09-19 13:04:58 +0200

    fixed paths to jobmodals component and accepted current changes

--- src/app/components/Careers/CareersHeader/CareersHeader.tsx
+++ src/app/components/Careers/CareersHeader/CareersHeader.tsx
@@ -11,13 +11,8 @@ export default function CareersHeader() {
         <div className='careersContainer'>
           <h1 className='title'>CAREERS</h1>
           <p className='description'>
-<<<<<<< HEAD
             We're excited to meet talented and passionate individuals who are
             eager to join our team. At Global Property, we offer exciting
-=======
-            We&apos;re excited to meet talented and passionate individuals who
-            are eager to join our team. At Global Property, we offer exciting
->>>>>>> dfb6415f97ae81c0234ef03ca43262dd22d1cca0
             opportunities to grow, innovate, and make a real impact in the world
             of real estate. Explore our open positions below and find the role
             that suits you best.
