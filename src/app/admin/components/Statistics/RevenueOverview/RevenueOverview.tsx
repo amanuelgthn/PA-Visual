a7731967d2f26725cf35f5944c47bc732ee5d8aa
Amanuel Getahun <amanuelgthn@gmail.com>
2024-09-19 16:46:14 +0200

    syles updated

--- src/app/admin/components/Statistics/RevenueOverview/RevenueOverview.tsx
+++ src/app/admin/components/Statistics/RevenueOverview/RevenueOverview.tsx
@@ -2,7 +2,30 @@ import React from 'react'
 import './RevenueOverview.scss'
 
 const RevenueOverview = () => {
-  return <div>RevenueOverview</div>
+  return (
+    <div className='revenue-overview'>
+      <div className='header'>
+        <h3>Revenue Overview</h3>
+        <div className='controls'>
+          <button className='control-btn'>Weekly</button>
+          <button className='control-btn active'>Monthly</button>
+          <button className='control-btn'>Annually</button>
+        </div>
+      </div>
+      <div className='chart'>
+        {/* You would integrate a charting library here like Chart.js or ApexCharts */}
+        <div className='chart-placeholder'>
+          <p>$2.05M</p>
+          {/* Placeholder for chart */}
+        </div>
+      </div>
+      <div className='year-selector'>
+        <button>&lt;</button>
+        <span>2024</span>
+        <button>&gt;</button>
+      </div>
+    </div>
+  )
 }
 
 export default RevenueOverview
