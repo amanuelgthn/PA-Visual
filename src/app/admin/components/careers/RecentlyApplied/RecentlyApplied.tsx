bd80817afe90690da36355d722258b996b70348a
GrazynaDachtera <grazynadachtera@gmail.com>
2024-09-30 18:30:32 +0200

    added changes

--- src/app/admin/components/careers/RecentlyApplied/RecentlyApplied.tsx
+++ src/app/admin/components/careers/RecentlyApplied/RecentlyApplied.tsx
@@ -3,6 +3,15 @@ import './RecentlyApplied.scss'
 import { CiLocationOn } from 'react-icons/ci'
 import Link from 'next/link'
 
+const jobPositions = [
+  'Cloud Engineer',
+  'Content Creator',
+  'Graphic Designer',
+  'HR assistant',
+]
+
+const Applicants = ['Sam Wheeler', 'Sam Wheeler', 'Sam Wheeler', 'Sam Wheeler']
+
 const RecentlyApplied = () => {
   return (
     <section className='card-wrapper'>
@@ -14,25 +23,26 @@ const RecentlyApplied = () => {
           <h1>Job position</h1>
           <h1>Applicant</h1>
         </div>
-        {/* Placeholder content since no data is needed */}
-        <div className='property-item'>
-          <div className='property-details'>
-            <div className='property-info'>
-              <Link href='#'>
-                <h4 className='property-name'>Job position</h4>
-              </Link>
-              <div className='property-location'>
-                <CiLocationOn color='rgba(255, 255, 255, 0.5)' size={15} />
-                <p>Sample Address</p>
+
+        {jobPositions.map((job, index) => (
+          <div key={index} className='property-item'>
+            <div className='property-details'>
+              <div className='property-info'>
+                <Link href='#'>
+                  <h4 className='property-name'>{job}</h4>
+                </Link>
+                <div className='property-location'>
+                  <CiLocationOn color='rgba(255, 255, 255, 0.5)' size={15} />
+                  <p>Sample Address</p>
+                </div>
               </div>
             </div>
+            <div className='client-details'>
+              <p className='client-name'>{Applicants[index]}</p>
+              <p className='client-email'>client@example.com</p>
+            </div>
           </div>
-          <div className='client-details'>
-            <p className='client-name'>Client Name</p>
-            <p className='client-email'>client@example.com</p>
-          </div>
-        </div>
-        {/* You can duplicate the .property-item block to add more entries if needed */}
+        ))}
       </div>
     </section>
   )
