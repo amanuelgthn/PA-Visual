78ccb7e45427d62d6f0ceb5e7f2df8cf75bc5167
Amanuel Getahun <amanuelgthn@gmail.com>
2024-10-09 13:49:03 +0200

    test

---
+++ src/app/Properties/NewListings/page.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import './newListingWrapper.scss'
+
+const NewListingspage = () => {
+  return <div className='newListingWrapper'>NewListings</div>
+}
+
+export default NewListingspage
